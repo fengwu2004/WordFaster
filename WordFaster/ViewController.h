b581559ee4a79d732007432b9e3226c428650a34
yl <yanli@yellfun.com>
2016-08-29 16:04:25 +0800

    Initial Commit

---
+++ WordFaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordFaster
+//
+//  Created by ky on 8/29/16.
+//  Copyright © 2016 yellfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
